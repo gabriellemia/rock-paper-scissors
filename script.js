227f61178a537a7ed9cf926887f9f14311787a98
gabriellemia <gabrielle@mia.com>
2023-02-22 14:17:51 +0000

    made some changes to game function and playround function, game now works in the console and it plays 5 rounds then announces winner, also added prompt for user input

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let choices = ['rock', 'paper', 'scissors'];
+
+game();
+
 function getComputerChoice() {
+    
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let playerSelection;
-let computerSelection;
-let result;
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerSelection.toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
-    result = "";
+function playRound() {
+
+
+    let playerSelection = prompt("Your move! Rock, Paper or Scissors?").toLowerCase();
+    let computerSelection = getComputerChoice();
     
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection =='rock')) {
 
+            console.log("You win!!" + playerSelection + " beats " + computerSelection);
             playerScore += 1;
-            return result = ("You win!!" + playerSelection + " beats " + computerSelection);
         
     }
 
     else if (playerSelection === computerSelection) {
-
-            return result = ("It's a tie!!  Try again");
+            
+            console.log("It's a tie!!  Try again");
     }
     
     else {
+            
+            console.log("You lose!! " + computerSelection + " beats " + playerSelection);
             computerScore +=1;
-            return result = ("You lose!! " + computerSelection + " beats " + playerSelection);
     }
 }
-    playerSelection = prompt ("Your move! Rock, Paper or Scissors?");
-    computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-    for (let i = 0; i <= 5; i++) {
-
-    if (playerScore > computerScore)
-        return result = ("You won the game, well done")
-
-    else if (computerScore > playerScore)
-        return result = ("You lost, better luck next time!")
-
-    else 
+    for (let i = 0; i < 5; i++) {
         playRound();
-
     }
+    
+    if (playerScore > computerScore)
+        console.log("You won the game, well done!");
+    else
+        console.log("You lost, better luck next time!");
+       
 }
+
+
